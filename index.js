a03ffc69e6ebcb049b367d50cfffa21635340f1d
abhi31-V <54107698+abhi31-V@users.noreply.github.com>
2020-11-05 16:24:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let r=document.getElementById('in');
+let s=''
+but=document.querySelectorAll('button');
+for(item of but)
+{
+    item.addEventListener('click',(e)=>
+    {
+
+       bt=e.target.innerText;
+     if(bt=='X')
+     {
+         bt='*';
+         s+=bt;
+         r.value=s;
+
+     }
+     else if(bt=='C'){
+         s=''
+    r.value='';
+
+
+     }
+     else if(bt=='=')
+     {
+         s=eval(s)
+         r.value=s
+
+     }
+     else{
+         s+=bt;
+         r.value=s;
+     }
+    })
+}
